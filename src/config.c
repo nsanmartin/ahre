4b850da360d91b57a4f5ea2627a88fa27e24b292
nsanmartin <nsanmartin@dc.uba.ar>
2025-12-20 12:42:40 -0300

    search config dir in the system

--- src/config.c
+++ src/config.c
@@ -1,5 +1,46 @@
+#include <sys/stat.h>
 #include "config.h"
 
+
+static Err _str_concat_mem_2z(char* m1, size_t l1, char* m2, size_t l2, Str out[_1_]) {
+    try(str_append(out, m1, l1));
+    try(str_append(out, m2, l2));
+    return str_append_lit__(out, "\0");
+}
+#define _str_concat_mem_lit_z__(M1, L1, Lit, Out) \
+    _str_concat_mem_2z(M1, L1, Lit, lit_len__(Lit), Out)
+
+static bool _create_dir_and_remove_z_(Str dirname[_1_]) {
+    int rv = mkdir(items__(dirname), 0700);
+    --dirname->len;
+    if (!len__(dirname)) return false;
+    if (rv == 0) return true;
+    return errno == EEXIST;
+}
+
+Err create_or_get_confdir(Str confdir[_1_]) {
+    str_reset(confdir);
+    char* xdg_config = getenv("XDG_CONFIG_HOME");
+    if (xdg_config) {
+        try (_str_concat_mem_lit_z__(xdg_config, strlen(xdg_config), "/ahre", confdir));
+        if (_create_dir_and_remove_z_(confdir)) return Ok;
+    }
+
+    str_reset(confdir);
+    char* home = getenv("HOME");
+    if (home) {
+        try (_str_concat_mem_lit_z__(home, strlen(home), "/.config/ahre", confdir));
+        if (_create_dir_and_remove_z_(confdir)) return Ok;
+
+        str_reset(confdir);
+        try (_str_concat_mem_lit_z__(home, strlen(home), "/.ahre", confdir));
+
+        if (_create_dir_and_remove_z_(confdir)) return Ok;
+    }
+
+    if (xdg_config || home) return err_fmt("could not create confdir: %s", strerror(errno));
+    return "could not create confdir, neither XDG_CONFIG_HOME not HOME env vars found";
+}
 
 Err resolve_bookmarks_file(const char* path, Str out[_1_]) {
     bool file_exists;
--- src/config.h
+++ src/config.h
@@ -6,20 +6,15 @@
 
 //TODO: cache all these in session-conf
 
-static inline Err get_config_dir(Str out[_1_]) {
-#define AHRE_CONFIG_SUBDIR "/.config/ahre"
-    char* homedir = getenv("HOME");
-    if (homedir) {
-        try( str_append(out, homedir, strlen(homedir)));
-        try( str_append_lit__(out, AHRE_CONFIG_SUBDIR));
-        return Ok;
-    }
-    return "conf dir not found";
+Err create_or_get_confdir(Str confdir[_1_]);
+
+static inline Err get_config_dir(Str confdir[_1_]) {
+    return create_or_get_confdir(confdir);
 }
 
 static inline Err get_cookies_filename(Str out[_1_]) {
 #define COOKIES_FILENAME "cookies.txt"
-    try( get_config_dir(out));
+    if (!len__(out)) return "config dir must not be empty";
     try( str_append_lit__(out, "/" COOKIES_FILENAME "\0"));
     return Ok;
 }
@@ -27,7 +22,7 @@ static inline Err get_cookies_filename(Str out[_1_]) {
 
 static inline Err get_input_history_filename(Str out[_1_]) {
 #define INPUT_HISTORY_FILENAME "input-history"
-    try( get_config_dir(out));
+    if (!len__(out)) return "config dir must not be empty";
     try( str_append_lit__(out, "/" INPUT_HISTORY_FILENAME "\0"));
     return Ok;
 }
@@ -35,23 +30,24 @@ static inline Err get_input_history_filename(Str out[_1_]) {
 
 static inline Err get_fetch_history_filename(Str out[_1_]) {
 #define FETCH_HISTORY_FILENAME "fetch-history"
-    try( get_config_dir(out));
+    if (!len__(out)) return "config dir must not be empty";
     try( str_append_lit__(out, "/" FETCH_HISTORY_FILENAME "\0"));
     return Ok;
 }
 
 static inline Err get_bookmark_filename(const char* fname, Str out[_1_]) {
+    if (!len__(out)) return "config dir must not be empty";
     if (fname) {
         if (!*fname) return "invalid file name for bookmark (\"\")";
         return str_append_z(out, (char*)fname, strlen(fname));
     }
 
-    try( get_config_dir(out));
     return str_append_lit__(out, "/bookmark.html\0");
 }
 
 static inline Err get_bookmark_filename_if_it_exists(const char* fname, Str out[_1_]) {
 #define FILE_SCHEMA "file://"
+    if (!len__(out)) return "config dir must not be empty";
     try( get_bookmark_filename(fname, out));
     if ((str_startswith_lit(out, FILE_SCHEMA) && file_exists(items__(out) + lit_len__(FILE_SCHEMA)))
         || file_exists(items__(out))
